87606e210f16f78a25f61fd5bd77fcbb1544a532
yarocruz <yarocruz@gmail.com>
2023-05-02 14:15:41 -0400

    changed to createChatCompletion api

--- helpers/get-colors.js
+++ helpers/get-colors.js
@@ -10,30 +10,21 @@ const openai = new OpenAIApi(configuration)
 
 async function getColors(query) {
 
-    const prompt = `
-    You are a color palette generating assistant that responds to text prompts for color palettes.
-    You should generate color palettes that fit the theme, mood, and instructions in the prompt. 
-    The palettes should be between 2 and 8 colors.
-    
-    Q: Convert the following verbal description of a color palette into a list of colors: The Mediterranean Sea.
-    A: ["#006699", "#9DC08B", "#F0E68C", "#008000", "#99CCFF", "#F08080"]
-    
-    Q: Convert the following verbal description of a color palette into a list of colors: sage, nature, earch
-    A: ["#9DC08B", "#609966", "#40513B"]
-    
-    Desired format: a JSON array of hex color codes, e.g. ["#006699", "#9DC08B", "#F0E68C", "#008000", "#99CCFF", "#F08080"]
-    
-    Text: ${query}.
-    
-    Result:
-    `
+    const messages = [
+        {"role": "system", "content": "You are a color palette generating assistant that responds to text prompts for color palettes. You should generate color palettes that fit the theme, mood, and instructions in the prompt. The palettes should be between 2 and 8 colors."},
+        {"role": "user", "content": "Convert the following verbal description of a color palette into a list of colors: The Mediterranean Sea."},
+        {"role": "assistant", "content": '["#006699", "#9DC08B", "#F0E68C", "#008000", "#99CCFF", "#F08080"]'},
+        {"role": "user", "content": "Convert the following verbal description of a color palette into a list of colors: sage, nature, earth"},
+        {"role": "assistant", "content": '["EDF1D6", "#9DC08B", "#609966", "#40513B"]'},
+        {"role": "user", "content": `Convert the following verbal description of a color palette into a list of colors: ${query}.`},
+    ]
 
-    const gptResponse = await openai.createCompletion({
-        model: 'text-davinci-003',
-        prompt: prompt,
+    const gptResponse = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: messages,
         max_tokens: 200,
     })
-    return gptResponse.data.choices[0].text
+    return gptResponse.data.choices[0].message.content
 }
 
 module.exports = getColors;
